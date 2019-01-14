9cb35932a6f768a81c78bc32c677acdcd5917d7d
TomasBTS <41930942+TomasBTS@users.noreply.github.com>
2019-01-14 21:51:29 +0000

    Add files via upload

---
+++ locales/br.lua
@@ -0,0 +1,11 @@
+Locales['br'] = {
+	['shop'] = 'Loja de comida',
+	['shops'] = 'loja de comida',
+	['press_menu'] = 'pressione ~INPUT_CONTEXT~ para acessar a loja.',
+	['bought'] = 'você acabou de comprar ~y~%sx~s~ ~b~%s~s~ por ~r~€%s~s~',
+	['not_enough'] = 'você não tem ~r~dinheiro suficiente~s~: %s',
+	['player_cannot_hold'] = 'você ~r~não~s~ tem bastante ~y~espaço livre~s~ no seu inventário!',
+	['shop_confirm'] = 'comprar %sx %s por €%s?',
+	['no'] = 'não',
+	['yes'] = 'sim',
+}
